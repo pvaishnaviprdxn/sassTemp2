45b302d0bbc1b87478094b93eaeeec1e9c5f0e19
pvaishnaviprdxn <vaishnavip@prdxn.com>
2020-06-04 16:50:07 +0530

    nav function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
-/* Author: 
+/* Author: */
 
-*/
-//let userinput = prompt("Enter any number");
-function mynum(){
-    let userinput = document.getElementById("input").value;
-    let usernum = userinput;
-    let num = [1,2,6,4,3,9,7,8,10,23,4,1,23,41,6,12,18,55,71,32];
-    let numlen = num.length;
-    let sum=0;
-    for(let i = 0;i < numlen;i++){
-        for(let j=i+1; j < numlen;j++){
-            sum = num[i] + num[j];
-            if(sum == usernum){
-                document.write(num[i]+"&"+num[j]+" ");
-            }
-        }
+var menu = document.querySelector(".hamburger");
+menu.addEventListener("click",navigation,false);
+/*navigation function*/
+function navigation() {
+    var nav = document.getElementsByTagName("nav")[0];
+    if(nav.style.maxHeight) {
+        nav.style.maxHeight = null;
+    }
+    else {
+        nav.style.maxHeight = nav.scrollHeight + "px";
+        nav.style.display = "block";
     }
 }
-
-
 
 
 
